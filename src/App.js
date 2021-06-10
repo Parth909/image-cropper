c7e338fe7a9662e5879d507f3223e3c9fe992d1a
Parth909 <parth.bhoir909@gmail.com>
2021-06-10 21:19:15 +0530

    The basic image-cropper app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CropImage from "./CropImage";
+
+const App = () => {
+  return (
+    <div className="App">
+      <CropImage />
+    </div>
+  );
+};
+
+export default App;
